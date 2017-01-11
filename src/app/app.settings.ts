e27fd82816dbca3bdab535f28270f0d4680e7ce4
eLetterBank <eletterbank@gmail.com>
2017-01-11 14:43:56 +1100

    greeting service created

---
+++ src/app/app.settings.ts
@@ -0,0 +1,3 @@
+export class AppSettings {
+   public static GREETING_API_ENDPOINT = 'http://10.0.0.44:8080/api/';
+}
--- src/app/greeting/greeting.component.ts
+++ src/app/greeting/greeting.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable }         from 'rxjs/Observable';
+
+import { Greeting }           from './greeting';
+import { GreetingService }    from './greeting.service';
+
 @Component({
   selector: 'app-greeting',
   templateUrl: './greeting.component.html',
   styleUrls: ['./greeting.component.css']
 })
+
 export class GreetingComponent implements OnInit {
+  //errorMessage: string;
+  queryResult: Greeting;
 
-  constructor() { }
+  constructor(private greetingService: GreetingService) { }
 
-  ngOnInit() {
+  ngOnInit() { this.queryGreeting(); }
+
+  queryGreeting() {
+    this.queryResult = new Greeting();
+    this.greetingService.getGreeting().then(data=>this.queryResult.data=data);
   }
-
 }
